3423fa375cade63f5950c171ddadda5b9d94bd4b
Chumakov Ilya <aidforsoft@mail.ru>
2016-11-16 22:40:00 +0300

    sqlite runtime web fix.

--- Bringo.HotDeliveryService.Web/App_Start/WebApiConfig.cs
+++ Bringo.HotDeliveryService.Web/App_Start/WebApiConfig.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Formatting;
 using System.Web.Http;
 
 namespace Bringo.HotDeliveryService.Web
@@ -19,6 +20,13 @@ namespace Bringo.HotDeliveryService.Web
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            GlobalConfiguration.Configuration.Formatters.JsonFormatter.MediaTypeMappings
+                .Add(new RequestHeaderMapping("Accept",
+                                              "text/html",
+                                              StringComparison.InvariantCultureIgnoreCase,
+                                              true,
+                                              "application/json"));
         }
     }
 }
